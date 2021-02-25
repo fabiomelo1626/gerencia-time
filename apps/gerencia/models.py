dcf5035811bd792feef3aa3fb32ca1db8c4738b1
fabio <fabiomelo.1626@gmail.com>
2021-02-24 23:34:47 -0300

    atualização de cont para partidas na tabela de jogador e scores

--- apps/gerencia/models.py
+++ apps/gerencia/models.py
@@ -17,25 +17,25 @@ class Scores(models.Model):
 
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
         # Salvar os dados
-        self.jogador.cont += 1
-        self.jogador.acerto_passe_curto = self.acerto_passe_curto
-        self.jogador.media_acert_pass_curt = self.jogador.acerto_passe_curto / self.jogador.cont
-        self.jogador.acerto_passe_longo = self.acerto_passe_longo
-        self.jogador.media_acert_pass_long = self.jogador.acerto_passe_longo / self.jogador.cont
-        self.jogador.gols = self.gols
-        self.jogador.media_gosl = self.jogador.gols / self.jogador.cont
-        self.jogador.desarmes = self.desarmes
-        self.jogador.media_desarmes = self.jogador.desarmes / self.jogador.cont
-        self.jogador.defesas = self.defesas
-        self.jogador.media_defesas = self.jogador.defesas / self.jogador.cont
-        self.jogador.passes_gol = self.passes_gol
-        self.jogador.media_pass_gol = self.jogador.passes_gol / self.jogador.cont
-        self.jogador.faltas = self.faltas
-        self.jogador.media_faltas = self.jogador.faltas / self.jogador.cont
-        self.jogador.cartao_amarelo = self.cartao_amarelo
-        self.jogador.media_cart_amar = self.jogador.cartao_amarelo / self.jogador.cont
-        self.jogador.cartao_vermelho = self.cartao_vermelho
-        self.jogador.media_cart_verm = self.jogador.cartao_vermelho / self.jogador.cont
+        self.jogador.partidas += 1
+        self.jogador.acerto_passe_curto += self.acerto_passe_curto
+        self.jogador.media_acert_pass_curt = self.jogador.acerto_passe_curto / self.jogador.partidas
+        self.jogador.acerto_passe_longo += self.acerto_passe_longo
+        self.jogador.media_acert_pass_long = self.jogador.acerto_passe_longo / self.jogador.partidas
+        self.jogador.gols += self.gols
+        self.jogador.media_gosl = self.jogador.gols / self.jogador.partidas
+        self.jogador.desarmes += self.desarmes
+        self.jogador.media_desarmes = self.jogador.desarmes / self.jogador.partidas
+        self.jogador.defesas += self.defesas
+        self.jogador.media_defesas = self.jogador.defesas / self.jogador.partidas
+        self.jogador.passes_gol += self.passes_gol
+        self.jogador.media_pass_gol = self.jogador.passes_gol / self.jogador.partidas
+        self.jogador.faltas += self.faltas
+        self.jogador.media_faltas = self.jogador.faltas / self.jogador.partidas
+        self.jogador.cartao_amarelo += self.cartao_amarelo
+        self.jogador.media_cart_amar = self.jogador.cartao_amarelo / self.jogador.partidas
+        self.jogador.cartao_vermelho += self.cartao_vermelho
+        self.jogador.media_cart_verm = self.jogador.cartao_vermelho / self.jogador.partidas
         self.jogador.save()
         super(Scores, self).save(force_insert, force_update, *args, **kwargs)
 
